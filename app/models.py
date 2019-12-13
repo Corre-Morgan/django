fd393d984a36a63bde61a01c019c0a256acff8b4
Corre-Morgan <morgan.corre@etu.univ-amu.fr>
2019-12-13 16:08:43 +0100

    first commit

---
+++ app/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+
+
+class Cocktail(models.Model):
+    title = models.CharField(max_length=200, blank=True, null=True, default='(no title)')
+    description = models.TextField(blank=True, null=True, default=None)
+    recipe = models.TextField(blank=True, default=True)
+    tags = models.ManyToManyField('Tag', related_name='cocktails', through='CocktailTag')
+
+    def __str__(self):
+        return self.title if self.title is not None else '????????'
+
+
+class CocktailTag(models.Model):
+    cocktail = models.ForeignKey(Cocktail, on_delete=models.CASCADE)
+    tag = models.ForeignKey('Tag', on_delete=models.CASCADE)
+
+
+class Unit(models.Model):
+    description = models.CharField(max_length=200, blank=True, null=True, default='(no title)')
+
+    def __str__(self):
+        return self.description if self.description is not None else '????????'
+
+
+class Ingredient(models.Model):
+    description = models.CharField(max_length=200, blank=True, null=True, default='(no title)')
+
+    def __str__(self):
+        return self.description if self.description is not None else '????????'
+
+
+class CocktailIngredientUnit(models.Model):
+    cocktail = models.ForeignKey(Cocktail, on_delete=models.CASCADE, default=None, null=True, related_name='ingredients')
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE, default=None, null=True)
+    value = models.FloatField(default=1.0, blank=False)
+    unit = models.ForeignKey(Unit, on_delete=models.CASCADE, default=None, null=True)
+
+    def __str__(self):
+        return '{} {} {}'.format(self.value, self.unit, self.ingredient)
+
+
+class Tag(models.Model):
+    description = models.CharField(max_length=200, blank=True, null=True, default='(no title)')
